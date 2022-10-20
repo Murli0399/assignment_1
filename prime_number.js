7182c8b679a222b652e904018ec6b48323cd2429
Murli0399 <murlikhaire28@gmail.com>
2022-10-20 20:22:16 +0530

    prime number

---
+++ prime_number.js
@@ -0,0 +1,15 @@
+let n=29;
+let count=0;
+
+for(let i=1;i<=n;i++){
+  if(n%i==0||i%n==0){
+    count++;
+  }
+}
+
+if(count==2){
+  console.log(n,"is a prime number");
+}
+else{
+  console.log(n,"is a not prime number");
+}
